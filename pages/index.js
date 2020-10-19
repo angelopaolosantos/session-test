759acc3747f5e3cb38b17a3d718b846b7cb6f11f
angelopaolosantos <angelopaolosantos@gmail.com>
2020-10-18 19:05:31 -0700

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,50 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Cookies from 'js-cookie'
+import { useState, useEffect } from 'react'
+
+const isBrowser = () => typeof window !== "undefined"
 
 export default function Home() {
+
+  const [name, setName] = useState("")
+  const [wst, setWST] = useState("")
+
+  useEffect(()=>{
+    if (isBrowser()) {
+      setName(Cookies.get("name"))
+      setWST(window.sessionStorage.getItem("windowSessionTest"))
+    }
+  }, [])
+
+  const setCookie = () => {
+    Cookies.set('name', 'John Doe')
+    window.sessionStorage.setItem("windowSessionTest", "123")
+
+    updateName()
+  }
+
+  const unSetCookie = () => {
+    Cookies.remove('name')
+    window.sessionStorage.removeItem("windowSessionTest");
+
+    updateName()
+  }
+
+  const updateName = () => {
+    if (isBrowser()) {
+      setName(Cookies.get("name"))
+      setWST(window.sessionStorage.getItem("windowSessionTest"))
+    }
+  }
+
+
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      Name: {name}<br />
+      Window Session Test: {wst}<br />
+      <button onClick={setCookie}>Set Cookie</button><br />
+      <button onClick={unSetCookie}>Unset Cookie</button>
     </div>
   )
 }
